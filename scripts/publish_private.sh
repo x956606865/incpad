0c27a64454b77bcba1dc2fb57bc3dcae6b77114b
aj0k3r <x3359449@qq.com>
2018-12-07 11:34:17 +0800

    build(script): update shell

--- scripts/publish_private.sh
+++ scripts/publish_private.sh
@@ -1,2 +1,8 @@
+#强制登录
 npm login --registry=http://ec2-18-221-5-8.us-east-2.compute.amazonaws.com:7001
+#发布模块到仓库
 lerna publish --registry=http://ec2-18-221-5-8.us-east-2.compute.amazonaws.com:7001
+#获取分支名称
+brach_name=`git symbolic-ref --short -q HEAD`
+#auto upload
+npm run release
