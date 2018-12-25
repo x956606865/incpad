797f223542c2f48356a3219b40edfb4b6fd84f6f
XiaBin <XiaBin@inceptionpad.com>
2018-12-25 17:02:47 +0800

    feat(storybook): add new story(Button with icon) to storybook

--- packages/storybook/stories/Button.stories.js
+++ packages/storybook/stories/Button.stories.js
@@ -69,4 +69,21 @@ storiesOf('Button')
                 loading={boolean('loading', true)}
             />
         </div>
+    ))
+    .add('withIcon', () => (
+        <div style={style.container}>
+            <Button type="primary" shape="circle" icon="search" />
+            <Button type="primary" icon="search">
+                Search
+            </Button>
+            <Button shape="circle" icon="search" />
+            <Button icon="search">Search</Button>
+            <br />
+            <Button shape="circle" icon="search" />
+            <Button icon="search">Search</Button>
+            <Button type="dashed" shape="circle" icon="search" />
+            <Button type="dashed" icon="search">
+                Search
+            </Button>
+        </div>
     ));
