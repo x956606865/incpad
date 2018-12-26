bb45ef1d9f4ce213b6400efac56721aea69a8046
XiaBin <XiaBin@inceptionpad.com>
2018-12-26 15:58:37 +0800

    refactor(storybook): update babel from 6->7 install some new addons

--- packages/storybook/.storybook/webpack.config.js
+++ packages/storybook/.storybook/webpack.config.js
@@ -27,6 +27,11 @@ module.exports = (baseConfig, env, defaultConfig) => {
         loaders: [require.resolve('@storybook/addon-storysource/loader')],
         enforce: 'pre',
     });
+    defaultConfig.resolve.extensions.push('.web.js');
+    defaultConfig.resolve.extensions.push('.web.jsx');
+    defaultConfig.resolve.alias['react-native'] = 'react-native-web';
+    console.log(defaultConfig.resolve);
+
     // defaultConfig.resolve.extensions.push(".ts", ".tsx");
 
     return defaultConfig;
